ff15340dd96f92ebb02dfb2c92ebc9ef1edbd499
yumamitani <ymsy.w.life3@icloud.com>
2019-10-28 16:56:38 +0900

    fix messages.js
    refactoring let buildMessageHTML = function(message)  in messages.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,55 +1,26 @@
 $(function(){
   let buildMessageHTML = function(message) {
-    if (message.content && message.image.url) {
-      var html = '<div class="massage" data-id=' + message.id + '>' +
-        '<div class="massage__top">' +
-          '<div class="massage__top__name">' +
-            message.user_name +
-          '</div>' +
-          '<div class="massage__top__day-time">' +
-            message.created_at +
-          '</div>' +
-        '</div>' +
-        '<div class="lower-message">' +
-          '<p class="lower-message__content">' +
-            message.content +
-          '</p>' +
-          '<img src="' + message.image.url + '" class="lower-message__image" >' +
-        '</div>' +
-      '</div>'
-    } else if (message.content) {
-      var html = '<div class="massage" data-id=' + message.id + '>' +
-      '<div class="massage__top">' +
-        '<div class="massage__top__name">' +
-          message.user_name +
-        '</div>' +
-        '<div class="massage__top__day-time">' +
-          message.created_at +
-        '</div>' +
-      '</div>' +
-      '<div class="lower-message">' +
-        '<p class="lower-message__content">' +
-          message.content +
-        '</p>' +
-      '</div>' +
-    '</div>'
-    } else if (message.image.url) {
-      var html = '<div class="massage" data-id=' + message.id + '>' +
-      '<div class="massage__top">' +
-        '<div class="massage__top__name">' +
-          message.user_name +
-        '</div>' +
-        '<div class="massage__top__day-time">' +
-          message.created_at +
-        '</div>' +
-      '</div>' +
-      '<div class="lower-message">' +
-        '<img src="' + message.image.url + '" class="lower-message__image" >' +
-      '</div>' +
-    '</div>'
-    };
-    return html;
-  };
+
+    image = (message.image.url) ? `<img class= "lower-message__image" src=${message.image.url} >` : "";
+    
+      let html = `<div class="massage" data-id="${message.id}">
+      <div class="massage__top">
+          <div class="massage__top__name">
+            ${message.user_name}
+          </div>
+          <div class="massage__top__day-time">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="lower-message">
+          <p class="lower-message__content">
+            ${message.content}
+          </p>
+          ${image} 
+        </div>
+      </div>`
+     return html;
+  }
 
   $('#new_message').on('submit', function(e){
     e.preventDefault();
@@ -90,7 +61,7 @@ $(function(){
         data: {last_id: last_message_id} 
       })
       .done(function (messages) { 
-        var insertHTML = '';
+        let insertHTML = '';
         messages.forEach(function (message) {
           insertHTML = buildMessageHTML(message); 
           $('.massages').append(insertHTML);
