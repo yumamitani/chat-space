f8ed2bcc00eb9b1ee11933d6fa1b00e0287faf9e
yumamitani <ymsy.w.life3@icloud.com>
2019-11-02 17:00:41 +0900

    fix auto uproad function & time zone

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -3,7 +3,7 @@ $(function(){
 
     image = (message.image.url) ? `<img class= "lower-message__image" src=${message.image.url} >` : "";
     
-      let html = `<div class="massage" data-id="${message.id}">
+      let html = `<div class="massage" data-message-id="${message.id}">
       <div class="massage__top">
           <div class="massage__top__name">
             ${message.user_name}
@@ -52,8 +52,8 @@ $(function(){
 
   var reloadMessages = function () {
     if (window.location.href.match(/\/groups\/\d+\/messages/)){
-      var last_message_id = $('.message:last').data("message-id"); 
-      
+      var last_message_id = $('.massage:last').data("message-id"); 
+      console.log(last_message_id)
       $.ajax({
         url: "api/messages", 
         type: 'get', 
